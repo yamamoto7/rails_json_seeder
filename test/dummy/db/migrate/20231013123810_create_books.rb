1e5c423110641610827811ae98b58f2ab3ae11df
kenta.yamamoto <yamamoto7.pgming@gmail.com>
2023-10-13 22:46:10 +0900

    add: sample seed config

---
+++ test/dummy/db/migrate/20231013123810_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :books do |t|
+      t.integer :category_id
+
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
