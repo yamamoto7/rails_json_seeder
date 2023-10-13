cfaaaf03a4faad3fbb0558ebfa1aedb3b0e671c3
kenta.yamamoto <yamamoto7.pgming@gmail.com>
2023-10-13 22:47:15 +0900

    feature: load_config

--- lib/rails_json_seeder.rb
+++ lib/rails_json_seeder.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "rails_json_seeder/version"
+require_relative 'rails_json_seeder/version'
+require_relative 'rails_json_seeder/seeder'
 
+# Main module for RailsJsonSeeder
 module RailsJsonSeeder
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def load_seeds(config_path)
+      puts 'Loading seeds by RailsJsonSeeder...'
+      Seeder.new.load_seeds(config_path)
+    end
+  end
 end
